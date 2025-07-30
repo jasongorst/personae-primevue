4ae822b5cd6eb4cce77da798d6ac5a2cbb1ebde0
Jason Gorst <jason.gorst@me.com>
2025-07-30 14:22:41 -0500

    switch useCharactersStore to pinia setup api, move api calls, query, and filter management into store, add patch() action

--- stores/useCharactersStore.js
+++ stores/useCharactersStore.js
@@ -1,28 +1,107 @@
-import initialFilters from "../utils/initialFilters.js"
+import * as jsonpatch from "fast-json-patch"
 
-// noinspection JSUnresolvedReference
-export const useCharactersStore = defineStore('characters', {
-  state: () => ({
-    data: {},
-    query: "",
-    filters: initialFilters,
-    sort: { attribute: "createdAt", order: "asc" }
-  }),
+export const useCharactersStore = defineStore("characters", () => {
+  const config = useRuntimeConfig()
 
-  getters: {
-    characters: (state) => _values(state.data),
-    count: (state) => _size(state.date),
-    isLoaded: (state) => isPositive(state.count),
-    hasFilter: (state) => _some(state.filters, (value) => isPresent(value.value))
-  },
+  // state
+  const data = ref({})
+  const filters = ref(initialFilters)
+  const sort = ref({ attribute: "createdAt", order: "asc" })
+  const loading = ref(false)
+  const error = ref(null)
 
-  actions: {
-    clearFilters() {
-      this.filters = initialFilters
-    },
+  // getters
+  const characters = computed(() => _values(data.value))
+  const count = computed(() => _size(characters.value))
+  const isLoaded = computed(() => isPositive(count.value))
+  const hasAnyFilter = computed(() => _some(filters.value, (value) => isPresent(value.value)))
+  const hasGlobalFilter = computed(() => isPresent(filters.value['global'].value))
+  const hasAnyAttributeFilter = computed(() => _some(_map(listAttributes, (attribute) => hasFilterFor(attribute))))
 
-    removeFilter(attribute, value) {
-      this.filters[attribute] = _without(this.filters[attribute], value)
+  const options = computed(
+    () => _fromPairs(_map(
+      optionsAttributes,
+      (attribute) => [
+        attribute,
+        _sortBy(_uniq(_compact(_map(characters.value, (character) => character[attribute]))))
+      ]
+    ))
+  )
+
+  // actions
+  async function _fetch() {
+    try {
+      const response = await $fetch(
+        "/characters",
+        { baseURL: config.public.api.baseURL }
+      )
+
+      data.value = deepParseTimestamps(deepConvertKeys(response, _camelCase))
+    } catch(err) {
+      console.log(err)
+
+      error.value = err
     }
-  },
+  }
+
+  async function loadCharacters() {
+    loading.value = true
+    await _fetch()
+    loading.value = false
+  }
+
+  async function reloadCharacters() {
+    await _fetch()
+  }
+
+  function hasFilterFor(attribute) {
+    return isPresent(filters.value[attribute]?.value)
+  }
+
+  function resetFilters() {
+    filters.value = initialFilters
+  }
+
+  function resetFilterFor(attribute) {
+    filters.value[attribute] = initialFilters[attribute]
+  }
+
+  function removeFilter(attribute, value) {
+    filters.value[attribute] = _without(filters.value[attribute], value)
+  }
+
+  function patch(patch) {
+    try {
+      jsonpatch.applyPatch(data.value, patch)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  return {
+    // state
+    data,
+    filters,
+    sort,
+    loading,
+    error,
+
+    // getters
+    characters,
+    count,
+    isLoaded,
+    hasAnyFilter,
+    hasGlobalFilter,
+    hasAnyAttributeFilter,
+    options,
+
+    // actions
+    loadCharacters,
+    reloadCharacters,
+    hasFilterFor,
+    resetFilters,
+    resetFilterFor,
+    removeFilter,
+    patch
+  }
 })
