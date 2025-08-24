ad51d0bd402b88988c0d2eb9dabb79714c05ae92
Jason Gorst <jason.gorst@me.com>
2025-08-23 23:08:00 -0500

    add plainTextAttributes, move emptyCharacter and emptyFilters

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,4 +1,6 @@
-export const detailAttributes = [
+import { FilterMatchMode } from "@primevue/core/api"
+
+const attributes = [
   "faeName",
   "mortalName",
   "player",
@@ -11,25 +13,58 @@ export const detailAttributes = [
   "position"
 ]
 
-const names = [ "faeName", "mortalName" ]
+const nameAttributes = [ "faeName", "mortalName" ]
 const omitFromList = [ "house", "bannerhouse" ]
 
 export const richTextAttributes = [ "description", "notes" ]
+export const plainTextAttributes = _map(richTextAttributes, (attribute) => `${attribute}PlainText`)
 
-// for characterList, all detail attributes except house and bannerhouse
-export const listAttributes = _difference(detailAttributes, omitFromList)
+// for datatable, all detail attributes except house and bannerhouse
+export const listAttributes = _difference(attributes, omitFromList)
 
 // for autocomplete, all detail attributes except names
-export const optionsAttributes = _difference(detailAttributes, names)
-
-// for filters, all list attributes except names
-export const categoryAttributes = _difference(listAttributes, names)
+export const optionsAttributes = _difference(attributes, nameAttributes)
 
 // for create/edit
 export const apiAttributes = _concat(
-  _map(names, (attr) => ({ attribute: attr, type: "text" })),
+  _map(nameAttributes, (attr) => ({ attribute: attr, type: "text" })),
   _map(optionsAttributes, (attr) => ({ attribute: attr, type: "autocomplete" })),
   _map(richTextAttributes, (attr) => ({ attribute: attr, type: "richText" }))
 )
 
-export const apiAttributesList = _map(apiAttributes, "attribute")
+// for global filter
+export const apiAttributesList = _concat(nameAttributes, optionsAttributes, plainTextAttributes)
+
+// for attribute filters, all list attributes except names
+export const categoryAttributes = _difference(listAttributes, nameAttributes)
+
+export const emptyCharacter = _fromPairs(
+  _map(apiAttributes, ({ attribute }) => [ attribute, null ])
+)
+
+export const emptyFilters = _reduce(
+  listAttributes,
+  (filters, attribute) => {
+    if (_includes(categoryAttributes, attribute)) {
+      // category attribute
+      filters[attribute] = {
+        value: [],
+        matchMode: FilterMatchMode.IN
+      }
+    } else {
+      filters[attribute] = {
+        value: "",
+        matchMode: FilterMatchMode.CONTAINS
+      }
+    }
+
+    // add global field
+    filters['global'] = {
+      value: "",
+      matchMode: FilterMatchMode.CONTAINS
+    }
+
+    return filters
+  },
+  {}
+)
