51900e283a92586a434b286284b880aedd946b75
Jason Gorst <jason.gorst@me.com>
2025-08-24 13:32:43 -0500

    add middleware to redirect from edit-id to show-id if not logged in

---
+++ app/middleware/rowSelect.js
@@ -0,0 +1,8 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const { status } = useAuth()
+  const isLoggedIn = computed(() => status.value === "authenticated")
+
+  if (!isLoggedIn.value) {
+    return navigateTo({ name: "show-id", params: { id: to.params.id } })
+  }
+})
