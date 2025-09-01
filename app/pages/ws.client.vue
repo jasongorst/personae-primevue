185d05cd4a08e6f08e71774b7c11f957250b32f2
Jason Gorst <jason.gorst@me.com>
2025-09-01 16:54:04 -0500

    websocket connection test

---
+++ app/pages/ws.client.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Status: {{ isConnected ? "connected" : "disconnected" }}</p>
+    <p>Transport: {{ transport }}</p>
+  </div>
+</template>
+
+<script setup>
+const { socket, isConnected, transport, onConnect, onDisconnect } = useSocket({
+  onConnectCallback: () => { console.log("connect callback") },
+  onDisconnectCallback: () => { console.log("disconnect callback") }
+})
+
+onBeforeUnmount(() => {
+  socket.off("connect", onConnect)
+  socket.off("disconnect", onDisconnect)
+})
+</script>
+
+<style scoped>
+
+</style>
