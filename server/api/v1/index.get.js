b88edda59dc4a277ce750a21b5b54bb11967b647
Jason Gorst <jason.gorst@me.com>
2025-09-01 16:49:29 -0500

    key characters by id

--- server/api/v1/index.get.js
+++ server/api/v1/index.get.js
@@ -1,8 +1,32 @@
+import { forEach, set, reduce } from "lodash-es"
+
 export default defineEventHandler(async () => {
   try {
-    return await prisma.character.findMany({
+    const characters = await prisma.character.findMany({
       orderBy: [{ createdAt: "asc" }]
     })
+    
+    return reduce(
+      // collection
+      characters,
+      
+      // iteratee
+      (accumulator, character) => {
+        // add plain text versions of rich text attributes to character
+        forEach(
+          richTextAttributes,
+          (attribute) => set(character, `${attribute}PlainText`, toPlainText(character[attribute]))
+        )
+        
+        // add character to accumulator, keyed by id
+        set(accumulator, character.id, character)
+        
+        return accumulator
+      },
+      
+      // initial accumulator
+      {}
+    )
   } catch (error) {
     throw createError({
       statusCode: 500,
