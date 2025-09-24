93702edc93977d72e2172cb793c73beebda8d95a
Jason Gorst <jason.gorst@me.com>
2025-09-23 23:32:59 -0500

    update for async load()

--- app/app.vue
+++ app/app.vue
@@ -5,41 +5,13 @@
 </template>
 
 <script setup>
-// sign in
-onBeforeMount(async () => {
-  const toast = useToast()
-  const { signIn } = useAuth()
-
-  const credentials = {
-    email: "jason@evilpaws.org",
-    password: "Y.nQ!Xw.L3b9288twbh-dxY6nq7yoHp."
-  }
-
-  await signInWithPassword(credentials)
-
-  async function signInWithPassword(values) {
-    try {
-      await signIn(values, { redirect: false })
-    } catch (error) {
-      console.log("[signIn error]", error)
-
-      toast.add({
-        severity: "error",
-        summary: "Not Signed In.",
-        detail: error.data?.message || error.message,
-        closeable: false
-      })
-    }
-  }
-})
-
 // register websocket handlers (client only)
 if (import.meta.client) {
   useWebsocketHandlers()
 }
 
 // init charactersStore
-callOnce(() => useCharactersStore().load())
+callOnce(async () => await useCharactersStore().load())
 </script>
 
 <style scoped>
