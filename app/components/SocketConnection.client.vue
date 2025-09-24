8d84f6dff323ac29bf6abf215a24c9c9b57b2397
Jason Gorst <jason.gorst@me.com>
2025-09-23 23:42:55 -0500

    dev component

---
+++ app/components/SocketConnection.client.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Status: {{ isConnected ? "connected" : "disconnected" }}</p>
+    <p>Transport: {{ transport }}</p>
+  </div>
+</template>
+
+<script setup>
+const { $socketio: { socket, isConnected, transport } } = useNuxtApp()
+
+onMounted(() => socket.prependAny((eventName, ...args) => {
+  console.log("[ws event]", eventName, args)
+}))
+
+onBeforeUnmount(() => socket.offAny())
+</script>
+
+<style scoped>
+
+</style>
