2d6f3137d4cf1290330f63f75120e33240e37429
Jason Gorst <jason.gorst@me.com>
2025-09-23 23:41:50 -0500

    add global filter chip, use charactersStore directly

--- app/components/FilterChips.vue
+++ app/components/FilterChips.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="flex flex-row flex-wrap gap-1">
+    <template v-if="hasGlobalFilter">
+      <Chip
+        :label="`&ldquo;${filters.global.value}&rdquo;`"
+        removable
+        pt:root="bg-primary-100 dark:bg-primary-800 text-primary-800 dark:text-primary-0!"
+        :ptOptions="{ mergeProps: ptViewMerge }"
+      >
+        <template #removeicon>
+          <Icon
+            name="ph:x-bold"
+            class="p-0.5 rounded-full hover:bg-primary-200 dark:hover:bg-primary-600"
+            size="1.25em"
+            @click="resetGlobalFilter"
+          />
+        </template>
+      </Chip>
+    </template>
+
     <template
+      v-if="hasAnyAttributeFilters"
       v-for="attribute in categoryAttributes"
       :key="attribute"
     >
       <Chip
+        v-if="hasFilterFor(attribute)"
         v-for="value in filters[attribute].value"
         :label="value"
         removable
@@ -16,7 +36,7 @@
             name="ph:x-bold"
             class="p-0.5 rounded-full hover:bg-primary-200 dark:hover:bg-primary-600"
             size="1.25em"
-            @click="emit('remove', attribute, value)"
+            @click="removeFilter(attribute, value)"
           />
         </template>
       </Chip>
@@ -25,14 +45,9 @@
 </template>
 
 <script setup>
-const props = defineProps({
-  filters: {
-    type: Object,
-    required: true
-  }
-})
-
-const emit = defineEmits([ "remove" ])
+const charactersStore = useCharactersStore()
+const { filters, hasAnyAttributeFilters, hasGlobalFilter } = storeToRefs(charactersStore)
+const { hasFilterFor, removeFilter, resetGlobalFilter } = charactersStore
 </script>
 
 <style scoped>
