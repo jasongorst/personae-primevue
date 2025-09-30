6e2500297b61f13a6af2d422706af406351a5d85
Jason Gorst <jason.gorst@me.com>
2025-09-29 22:26:49 -0500

    cleanup

--- app/composables/useAutoSignIn.js
+++ app/composables/useAutoSignIn.js
@@ -5,7 +5,7 @@ export default async function useAutoSignIn() {
     email: "jason@evilpaws.org",
     password: "Y.nQ!Xw.L3b9288twbh-dxY6nq7yoHp."
   }
-  
+
   try {
     await signIn(credentials, { redirect: false })
   } catch (error) {
