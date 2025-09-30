89894bc78ce44a84ec4de22d24261c5089bc4ef3
Jason Gorst <jason.gorst@me.com>
2025-09-29 22:25:26 -0500

    rewrite findDifferences

--- app/utils/findDifferences.js
+++
@@ -1,8 +0,0 @@
-export default function findDifferences(source, target) {
-  return _fromPairs(
-    _filter(
-      _toPairs(toValue(target)),
-      ([ key, value ]) => !_isEqual(value, toValue(source)?.[key])
-    )
-  )
-}
