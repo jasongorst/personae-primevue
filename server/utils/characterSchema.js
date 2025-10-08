fc9dc40eec6174bb824256034d953f6b10a16548
Jason Gorst <jason.gorst@me.com>
2025-10-07 20:24:24 -0500

    flesh out schema

--- server/utils/characterSchema.js
+++ server/utils/characterSchema.js
@@ -1,4 +1,11 @@
-import { number, object, string } from "yup"
+import { boolean, date, mixed, number, object, string } from "yup"
+import { isNil } from "lodash-es"
+
+const nil = mixed().notRequired().test(
+  "is-nil",
+  "${path} must be null or undefined",
+  (value) => isNil(value)
+)
 
 export default object({
   id: number().positive().integer().required(),
@@ -13,5 +20,20 @@ export default object({
   house: string().trim().defined(),
   bannerhouse: string().trim().defined(),
   description: string().trim().defined(),
-  notes: string().trim().defined()
+  notes: string().trim().defined(),
+  descriptionPlainText: string().trim().defined(),
+  notesPlainText: string().trim().defined(),
+  locked: boolean().optional(),
+  
+  lockedBy: string().notRequired().when("locked", {
+    is: true,
+    then: (schema) => schema.trim().required(),
+    otherwise: () => nil
+  }),
+  
+  lockedAt: date().notRequired().when("locked", {
+    is: true,
+    then: (schema) => schema.required().default(new Date()),
+    otherwise: () => nil
+  })
 })
--- server/utils/updateCharacterSchema.js
+++ server/utils/updateCharacterSchema.js
@@ -1,16 +1,22 @@
-import { object, string } from "yup"
+// noinspection JSUnresolvedReference
+import { object, reach } from "yup"
 
 export default object({
-  player: string().trim().optional(),
-  mortalName: string().trim().optional(),
-  faeName: string().trim().optional(),
-  rank: string().trim().optional(),
-  kith: string().trim().optional(),
-  location: string().trim().optional(),
-  position: string().trim().optional(),
-  seeming: string().trim().optional(),
-  house: string().trim().optional(),
-  bannerhouse: string().trim().optional(),
-  description: string().trim().optional(),
-  notes: string().trim().optional()
+  player: reach(characterSchema, "player").optional(),
+  mortalName: reach(characterSchema, "mortalName").optional(),
+  faeName: reach(characterSchema, "faeName").optional(),
+  rank: reach(characterSchema, "rank").optional(),
+  kith: reach(characterSchema, "kith").optional(),
+  location: reach(characterSchema, "location").optional(),
+  position: reach(characterSchema, "position").optional(),
+  seeming: reach(characterSchema, "seeming").optional(),
+  house: reach(characterSchema, "house").optional(),
+  bannerhouse: reach(characterSchema, "bannerhouse").optional(),
+  description: reach(characterSchema, "description").optional(),
+  notes: reach(characterSchema, "notes").optional(),
+  descriptionPlainText: reach(characterSchema, "descriptionPlainText").optional(),
+  notesPlainText: reach(characterSchema, "notesPlainText").optional(),
+  locked: reach(characterSchema, "locked"),
+  lockedBy: reach(characterSchema, "lockedBy"),
+  lockedAt: reach(characterSchema, "lockedAt")
 })
