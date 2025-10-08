8f3ad934c7077e09f9cbb2f3198644adde609bb3
Jason Gorst <jason.gorst@me.com>
2025-10-07 20:13:32 -0500

    move labels into DetailField, expose activate method on children, add editRequest

--- app/components/DetailView.vue
+++ app/components/DetailView.vue
@@ -2,27 +2,17 @@
   <Card class="max-w-prose mx-auto">
     <template #content>
       <div class="flex flex-col gap-2">
-        <div
-          class="flex flex-col"
-          v-for="{ attribute, type } in schemaAttributes"
+        <DetailField
+          v-for="{ attribute, type } in detailAttributes"
+          v-model="character[attribute]"
           :key="attribute"
-        >
-          <label
-            :for="attribute"
-            class="ml-1 text-primary dark:text-primary text-sm"
-          >
-            {{ _startCase(attribute) }}
-          </label>
-
-          <AttributeDetail
-            v-model="character[attribute]"
-            :attribute="attribute"
-            :type="type"
-            :disabled="!editable"
-            :suggestions="suggestions[attribute]"
-            :ref="(component) => { attributes[attribute] = component }"
-          />
-        </div>
+          :attribute="attribute"
+          :type="type"
+          :disabled="!editable"
+          :suggestions="suggestions[attribute]"
+          @click.capture.stop="editRequest(attribute)"
+          :ref="(component) => detailFields[attribute] = component"
+        />
       </div>
     </template>
 
@@ -35,6 +25,7 @@
 </template>
 
 <script setup>
+defineExpose({ activate, reset })
 const character = defineModel()
 
 const props = defineProps({
@@ -44,12 +35,9 @@ const props = defineProps({
   }
 })
 
-defineExpose({ reset })
-
-const attributes = ref({})
-
-const charactersStore = useCharactersStore()
-const { options } = storeToRefs(charactersStore)
+const emit = defineEmits([ "editRequest" ])
+const detailFields = ref({})
+const { options } = storeToRefs(useCharactersStore())
 const suggestions = ref(_clone(options.value))
 
 async function reset() {
@@ -58,8 +46,21 @@ async function reset() {
   // noinspection JSUnresolvedReference
   _forEach(
     richTextAttributes,
-    (attribute) => attributes.value[attribute].reset()
+    (attribute) => detailFields.value[attribute].reset()
   )
+}
+
+function activate(attribute) {
+  // noinspection JSUnresolvedReference
+  detailFields.value[attribute].activate()
+}
+
+function editRequest(attribute) {
+  if (!props.editable) {
+    return false
+  }
+
+  emit("editRequest", attribute)
 }
 </script>
 
