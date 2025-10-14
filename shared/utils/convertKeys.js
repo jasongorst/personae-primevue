8ac7646419d69cac079d94e7b8b1f950b14faa68
Jason Gorst <jason.gorst@me.com>
2025-10-13 23:18:07 -0500

    clean up unused files

--- shared/utils/convertKeys.js
+++
@@ -1,6 +0,0 @@
-export default function convertKeys(object, converter) {
-  return _mapKeys(
-    object,
-    (_, key) => converter(key)
-  )
-}
---
+++ unused/shared/utils/convertKeys.js
@@ -0,0 +1,3 @@
+export default function convertKeys(object, converter) {
+  return _mapKeys(object, (_, key) => converter(key))
+}
