a462550a78e7ebc0a0096eadd2efb9ac85a6c311
Jason Gorst <jason.gorst@me.com>
2025-10-19 01:41:44 -0500

    return object

--- server/utils/validateId.js
+++ server/utils/validateId.js
@@ -1,7 +1,13 @@
+// noinspection JSUnresolvedReference
+
 import { reach } from "yup"
 
 export default function validateId(id) {
-    const idSchema = reach(characterSchema, "id")
-    // noinspection JSUnresolvedReference
-    return idSchema.validateSync(id)
+  const idSchema = reach(characterSchema, "id")
+
+  try {
+    return { validId: idSchema.validateSync(id) }
+  } catch (error) {
+    return { error }
+  }
 }
