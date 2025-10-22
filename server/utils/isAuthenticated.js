c987d032077c275d0853749bc272dfc5e88eda7b
Jason Gorst <jason.gorst@me.com>
2025-10-21 23:11:21 -0500

    throw errors

--- server/utils/isAuthenticated.js
+++ server/utils/isAuthenticated.js
@@ -1,17 +1,14 @@
-export default async function isAuthenticated(socket, callback, token) {
+export default async function isAuthenticated(socket, token) {
   // mutates socket.data
-  // returns true after successful auth
-  if (_isNil(token)) {
-    callback({ error: "missing token" })
-    return false
+  if (_isNil(token) || _isEmpty(token)) {
+    throw Error("missing token")
   }
-  
+
   socket.data = await authenticateToken(token)
-  
+
   if (_isNull(socket.data.token)) {
-    callback({ error: "invalid token" })
-    return false
+    throw Error("invalid token")
   }
-  
+
   return true
 }
--- server/utils/validateId.js
+++ server/utils/validateId.js
@@ -1,13 +1,7 @@
-// noinspection JSUnresolvedReference
-
 import { reach } from "yup"
 
 export default function validateId(id) {
   const idSchema = reach(characterSchema, "id")
-
-  try {
-    return { validId: idSchema.validateSync(id) }
-  } catch (error) {
-    return { error }
-  }
+  // noinspection JSUnresolvedReference
+  return idSchema.validateSync(id)
 }
