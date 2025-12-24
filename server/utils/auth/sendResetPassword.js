b56b32da6fa226ae5c0e3f825e425f7e6c79f0f3
Jason Gorst <jason.gorst@me.com>
2025-12-24 10:04:43 -0600

    async/await

--- server/utils/auth/sendResetPassword.js
+++ server/utils/auth/sendResetPassword.js
@@ -2,22 +2,23 @@ import * as postmark from "postmark"
 
 const config = useRuntimeConfig()
 
-export default function sendResetPassword({ user, url }) {
+export default async function sendResetPassword({ user, url }) {
   const postmarkClient = new postmark.ServerClient(config.postmarkServerToken)
 
-  postmarkClient
+  const response = await postmarkClient
     .sendEmail({
-      From: "admin@evilpaws.org",
+      From: "personae.evilpaws.org <admin@evilpaws.org>",
       To: user.email,
       Subject: "Password reset requested.",
       TextBody: `Click to reset your password: ${url}`
     })
-    .then((response) => {
-      console.log("Sending password reset email.")
-      console.log(response.To)
-      console.log(response.SubmittedAt)
-      console.log(response.Message)
-      console.log(response.MessageID)
-      console.log(response.ErrorCode)
-    })
+
+  console.log("Sending password reset email.")
+  console.log(response.To)
+  console.log(response.SubmittedAt)
+  console.log(response.Message)
+  console.log(response.MessageID)
+  console.log(response.ErrorCode)
+
+  return response
 }
