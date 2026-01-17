081345730d74f0cf48807bc9eb7ef18430c13ea4
Jason Gorst <jason.gorst@me.com>
2026-01-16 23:44:18 -0600

    add SelectField

---
+++ app/components/SelectField.vue
@@ -0,0 +1,23 @@
+<template>
+  <Select
+    v-model="model"
+    ref="select"
+  >
+    <template #value="{ value, placeholder }">
+      <div v-if="value">
+        {{ value }}
+      </div>
+
+      <div v-else>
+        {{ placeholder }}
+      </div>
+    </template>
+  </Select>
+</template>
+
+<script setup>
+const model = defineModel()
+const select = useTemplateRef("select")
+</script>
+
+<style scoped></style>
