8fc69658aee0aa91ba915c2365a6eaf16e9be334
Jason Gorst <jason.gorst@me.com>
2026-01-16 23:52:14 -0600

    add async context

--- app/middleware/signedIn.jsx
+++ app/middleware/signedIn.jsx
@@ -1,39 +1,43 @@
 import SignInDialog from "~/components/SignInDialog.vue"
 
 export default defineNuxtRouteMiddleware(async () => {
-  const { isSignedIn } = useAuthClient()
+  const nuxtApp = useNuxtApp()
+  const { fetchSession, isSignedIn } = useAuthClient()
+  await fetchSession()
 
   if (!isSignedIn.value) {
-    const dialog = useDialog()
+    return nuxtApp.runWithContext(() => {
+      const dialog = useDialog()
 
-    useOpenDialog(dialog, SignInDialog, {
-      props: {
-        showHeader: true,
-        pt: {
-          header: "pb-0",
-          pcCloseButton: {
-            root: "rounded-[1em]! size-6!",
-            icon: "size-3"
+      useOpenDialog(dialog, SignInDialog, {
+        props: {
+          showHeader: true,
+          pt: {
+            header: "pb-0",
+            pcCloseButton: {
+              root: "rounded-[1em]! size-6!",
+              icon: "size-3"
+            }
           }
-        }
-      },
+        },
 
-      templates: {
-        header: (
-          <div class="flex items-center gap-2 p-0 text-warn-600 dark:text-warn-500">
-            <Icon
-              name="ph:warning-circle-fill"
-              size="1.375rem"
-            />
+        templates: {
+          header: (
+            <div class="flex items-center gap-2 p-0 text-warn-600 dark:text-warn-500">
+              <Icon
+                name="ph:warning-circle-fill"
+                size="1.375rem"
+              />
 
-            <div class="text-lg font-semibold">
-              You&rsquo;ll need to sign in before doing that.
+              <div class="text-lg font-semibold">
+                You&rsquo;ll need to sign in before doing that.
+              </div>
             </div>
-          </div>
-        )
-      }
-    })
+          )
+        }
+      })
 
-    return abortNavigation()
+      return abortNavigation()
+    })
   }
 })
