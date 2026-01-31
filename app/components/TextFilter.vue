6cc49a499bb7eb7828f99f09a63199f3e08ecdea
Jason Gorst <jason.gorst@me.com>
2026-01-31 00:26:40 -0600

    add Clear button

--- app/components/TextFilter.vue
+++ app/components/TextFilter.vue
@@ -1,11 +1,24 @@
 <template>
-  <InputText
-    v-model="model"
-    fluid
-    type="search"
-    placeholder="Filter&hellip;"
-    @input="filterCallback"
-  />
+  <div class="flex min-h-56.5 flex-col justify-between gap-2">
+    <InputText
+      v-model="model"
+      fluid
+      type="search"
+      placeholder="Filter&hellip;"
+      @input="filterCallback"
+    />
+
+    <Button
+      class="py-0.5!"
+      variant="outlined"
+      size="small"
+      fluid
+      :disabled="!hasFilter()"
+      @click="resetFilter"
+    >
+      Clear
+    </Button>
+  </div>
 </template>
 
 <script setup>
@@ -13,6 +26,16 @@ const model = defineModel()
 
 const props = defineProps({
   filterCallback: {
+    type: Function,
+    required: true
+  },
+
+  hasFilter: {
+    type: Function,
+    required: true
+  },
+
+  resetFilter: {
     type: Function,
     required: true
   }
