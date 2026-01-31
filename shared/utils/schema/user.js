62623ad65756bc79c521a7886aa1b7deccfc9cbf
Jason Gorst <jason.gorst@me.com>
2026-01-31 00:23:13 -0600

    trim email, name, username

--- shared/utils/schema/user.js
+++ shared/utils/schema/user.js
@@ -4,15 +4,15 @@ zodConfig()
 
 export const userSchema = z.object({
   id: z.uuidv4("Invalid id."),
-  name: z.string().min(2),
-  email: z.email("Must be a valid email address."),
-  // emailVerified: z.boolean().default(false),
+  name: z.string().trim().min(2),
+  email: z.email("Must be a valid email address.").trim(),
   role: z.enum(["user", "admin"], "Must be either 'user' or 'admin'."),
-  username: z.string().min(2),
+  username: z.string().trim().min(2),
   password: z.string().min(12)
 })
 
 export const createUserSchema = userSchema.omit({ id: true })
+
 export const updateUserSchema = createUserSchema
   .omit({ password: true })
   .partial()
