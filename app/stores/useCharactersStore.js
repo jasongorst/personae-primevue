bfdafebd85b75cd8f025aa7df8c6b65a31707432
Jason Gorst <jason.gorst@me.com>
2026-02-08 12:47:35 -0600

    start using queryCache

--- app/stores/useCharactersStore.js
+++ app/stores/useCharactersStore.js
@@ -1,49 +1,39 @@
 import { FilterMatchMode } from "@primevue/core/api"
 
 export const useCharactersStore = defineStore("characters", () => {
-  const {
-    $socketio: { socket }
-  } = useNuxtApp()
-
-  // fetch characters
-  const {
-    // state
-    data,
-    error: loadError,
-    status: loadStatus,
-
-    // actions
-    refresh: load
-  } = useAsyncData(
-    "character:list",
-    async () => {
-      const { data: response } = await socket
-        .timeout(3000)
-        .emitWithAck("character:list")
-
-      return response
-    },
-    { deep: true }
-  )
+  const queryCache = useQueryCache()
 
   // state
+  const data = ref({})
 
-  // attribute groups
-  const categoryAttributes = ref(_pickBy(characterAttributes, { type: "autocomplete" }))
-  const datatableAttributes = ref(_pickBy(characterAttributes, { showColumn: true }))
+  const categoryAttributes = ref(
+    _pickBy(characterAttributes, { type: "autocomplete" })
+  )
+
+  const datatableAttributes = ref(
+    _pickBy(characterAttributes, { showColumn: true })
+  )
+
   const nameAttributes = ref(_pickBy(characterAttributes, { type: "text" }))
-  const datatableCategoryAttributes = ref(_pickBy(datatableAttributes.value, { type: "autocomplete" }))
+
+  const datatableCategoryAttributes = ref(
+    _pickBy(datatableAttributes.value, { type: "autocomplete" })
+  )
 
   // empty filters object
-  const emptyFilters = ref(_mapValues(
-    datatableAttributes.value,
-    ({ type }) =>
+  const emptyFilters = ref({
+    global: { value: "", matchMode: FilterMatchMode.CONTAINS },
+
+    ..._mapValues(datatableAttributes.value, ({ type }) =>
       type === "autocomplete"
         ? { value: [], matchMode: FilterMatchMode.IN }
         : { value: "", matchMode: FilterMatchMode.CONTAINS }
-  ))
+    )
+  })
 
-  emptyFilters.value.global = { value: "", matchMode: FilterMatchMode.CONTAINS }
+  // empty character
+  const emptyCharacter = ref(_mapValues(characterAttributes, () => ""))
+
 
   const filters = ref(_cloneDeep(emptyFilters.value))
   const sort = ref({ attribute: "createdAt", order: "asc" })
@@ -51,15 +41,20 @@ export const useCharactersStore = defineStore("characters", () => {
   // getters
   const characters = computed(() => _values(data.value))
   const count = computed(() => _size(characters.value))
-  const isLoaded = computed(() => loadStatus.value === "success")
-  const isLoading = computed(() => loadStatus.value === "pending")
-  const hasLoadError = computed(() => loadStatus.value === "error")
-
   const hasGlobalFilter = computed(() => isPresent(filters.value.global.value))
   const hasAnyFilters = computed(() => hasAnyFiltersFor(_keys(filters.value)))
-  const hasAnyNameFilters = computed(() => hasAnyFiltersFor(_keys(nameAttributes.value)))
-  const hasAnyCategoryFilters = computed(() => hasAnyFiltersFor(_keys(datatableCategoryAttributes.value)))
-  const hasAnyAttributeFilters = computed(() => hasAnyFiltersFor(_keys(datatableAttributes.value)))
+
+  const hasAnyNameFilters = computed(() =>
+    hasAnyFiltersFor(_keys(nameAttributes.value))
+  )
+
+  const hasAnyCategoryFilters = computed(() =>
+    hasAnyFiltersFor(_keys(datatableCategoryAttributes.value))
+  )
+
+  const hasAnyAttributeFilters = computed(() =>
+    hasAnyFiltersFor(_keys(datatableAttributes.value))
+  )
 
   const hasFilterByAttribute = computed(() =>
     _mapValues(filters.value, ({ value }) => isPresent(value))
@@ -73,9 +68,8 @@ export const useCharactersStore = defineStore("characters", () => {
 
   // actions
   function hasAnyFiltersFor(attributes) {
-    return _some(
-      _pick(filters.value, attributes),
-      ({ value }) => isPresent(value)
+    return _some(_pick(filters.value, attributes), ({ value }) =>
+      isPresent(value)
     )
   }
 
@@ -102,105 +96,57 @@ export const useCharactersStore = defineStore("characters", () => {
     filters.value.global.value = ""
   }
 
-  async function ensureLoaded() {
-    if (!isLoaded.value) {
-      await load()
+  function getCharacter(id) {
+    if (!isEmpty(data.value)) {
+      return data.value?.[id]
+    } else {
+      return null
     }
-  }
-
-  async function getCharacter(id) {
-    await ensureLoaded()
-    return data.value[id]
   }
 
   function applyPatch(patch) {
     // jsonPatch.patch(data.value, patch)
   }
 
-  async function read(id) {
-    const { data, error, status } = await useAsyncData(
-      `character:read:${id}`,
-      async () => {
-        const { data: response } = await socket
-          .timeout(3000)
-          .emitWithAck("character:read", id)
-        return response
-      }
-    )
-
-    if (status.value === "error") {
-      return { error }
-    }
-
-    return data.value
+  function load() {
+    data.value = queryCache.getQueryData(characterQuery.key)
   }
 
-  async function create(character) {
-    try {
-      const response = await socket
-        .timeout(3000)
-        .emitWithAck("character:create", character)
+  // async function read(id) {
+  //   const { data: character } = useQuery(characterByIdQuery, () => ({ id }))
+  //
+  //   return character
+  // }
 
-      if (_has(response, "data")) {
-        _set(data.value, response.data.id, response.data)
-      }
+  // async function create(character) {
+  //   const { createCharacter } = useCreateCharacter()
+  //
+  //   return createCharacter(character)
+  // }
 
-      return response
-    } catch (error) {
-      console.error("[useCharacterStore create]", error)
+  // async function update(id, attributes) {
+  //   const { updateCharacter } = useUpdateCharacter()
+  //
+  //   return updateCharacter(id, attributes)
+  // }
 
-      return { error }
-    }
-  }
-
-  async function update(id, character) {
-    try {
-      const response = await socket
-        .timeout(3000)
-        .emitWithAck("character:update", id, character)
-
-      if (_has(response, "data")) {
-        _set(data.value, response.data.id, response.data)
-      }
-
-      return response
-    } catch (error) {
-      console.error("[useCharacterStore update]", error)
-
-      return { error }
-    }
-  }
-
-  async function destroy(id) {
-    try {
-      const response = await socket
-        .timeout(3000)
-        .emitWithAck("character:delete", id)
-
-      if (_has(response, "data")) {
-        _unset(data.value, response.data.id)
-      }
-
-      return response
-    } catch (error) {
-      console.error("[useCharacterStore destroy]", error)
-
-      return { error }
-    }
-  }
+  // async function destroy(id) {
+  //   const { deleteCharacter } = useDeleteCharacter()
+  //
+  //   return deleteCharacter(id)
+  // }
 
   return {
     // state
     data,
     filters,
-    loadError,
-    loadStatus,
     sort,
     categoryAttributes,
     datatableAttributes,
     nameAttributes,
     datatableCategoryAttributes,
     emptyFilters,
+    emptyCharacter,
 
     // getters
     characters,
@@ -211,26 +157,22 @@ export const useCharactersStore = defineStore("characters", () => {
     hasAnyNameFilters,
     hasFilterByAttribute,
     hasGlobalFilter,
-    hasLoadError,
-    isLoaded,
-    isLoading,
     options,
 
     // actions
     hasAnyFiltersFor,
     applyPatch,
-    read,
-    create,
-    destroy,
-    ensureLoaded,
+    load,
+    // read,
+    // create,
+    // destroy,
     getCharacter,
     hasFilterFor,
-    load,
     removeFilterValueFrom,
     resetFilterFor,
     resetFilters,
     resetGlobalFilter,
-    update
+    // update
   }
 })
 
